29cb0a2bef167bff1533e499e82d3a91074a4a4b
juminbada <juminbada@sookmyung.ac.kr>
2023-06-20 23:30:46 +0900

    백엔드 개발18. @NamedQuery, @Query 사용하기

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -8,17 +8,15 @@ import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
-	
 	@Autowired
 	private TodoRepository repository;
-	
 	public String testService(){
-	//Todo Entity 생성
-	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+		// Todo Entity 생성
+		TodoEntity entity = TodoEntity.builder().userId("Jumin Jeong").title("My first todo item").build();
 	//Todo Entity 저장
 	repository.save(entity);
-	//Todo Entity 검색
-	TodoEntity savedEntity=	repository.findByUserId(entity.getUserId()).get(0);
+	// Todo Entity 검색
+	TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
 	return savedEntity.getUserId();
 	}
 }
